8ac4c7f3b3c63bcbfe84146325942067e5469b83
Alyson Barcellos Pegini <alyson.pegini@hbsis.com.br>
2020-09-22 18:33:01 -0300

    commi

--- BoletimMaroto/Controllers/AlunoController.cs
+++ BoletimMaroto/Controllers/AlunoController.cs
@@ -4,17 +4,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BoletimMaroto.Controllers
 {
-   // [ApiController]
-  //  [Route("Alunos")]
-   // public class AlunoController : ControllerBase
-//{
-       // [HttpPost]
-       // [Route("cadastroAlunos")]//inserir alunos
-       // public ActionResult AddAlunos(Aluno aluno)
-        //{
-        //    var result = new Result<Aluno>();
+    [ApiController]
+    [Route("Alunos")]
+    public class AlunoController : ControllerBase
+    {
+        [HttpPost]
+        [Route("cadastroAlunos")]//inserir alunos
+        public ActionResult AddAlunos(Aluno aluno)
+        {
+            var result = new Result<Aluno>();
 
-       // }
+        }
 
-   // }
+    }
 }
