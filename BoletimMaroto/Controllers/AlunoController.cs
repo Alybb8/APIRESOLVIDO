5e78dcf1410d7bcbd519418c3396abd6d0c5b9de
Alybb8 <70288424+Alybb8@users.noreply.github.com>
2020-09-22 18:27:40 -0300

    pri

---
+++ BoletimMaroto/Controllers/AlunoController.cs
@@ -0,0 +1,20 @@
+﻿using BoletimModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BoletimMaroto.Controllers
+{
+   // [ApiController]
+  //  [Route("Alunos")]
+   // public class AlunoController : ControllerBase
+//{
+       // [HttpPost]
+       // [Route("cadastroAlunos")]//inserir alunos
+       // public ActionResult AddAlunos(Aluno aluno)
+        //{
+        //    var result = new Result<Aluno>();
+
+       // }
+
+   // }
+}
